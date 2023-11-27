5f14a575dda5a20fcc939d747f9c768646291b86
Gouse <professionalgouse123@gmail.com>
2023-11-27 17:54:32 +0530

    initial commit

---
+++ src/OOPSConcepts/ConstructorOverloading.java
@@ -0,0 +1,48 @@
+package OOPSConcepts;
+
+import java.util.*;
+
+   // adding two numbers
+
+public class ConstructorOverloading
+{
+	int x,y;
+	double d;
+	
+	ConstructorOverloading()
+	{
+		x=10;
+		y=20;
+		System.out.println(x+y);	
+	}
+	
+	ConstructorOverloading(int a,int b)
+	{
+		x=a;
+		y=b;
+	    System.out.println(x+y);	
+	}
+
+	ConstructorOverloading(int a,double b)
+	{
+		x=a;
+		y=d;
+		System.out.println(x+d);	
+	}
+	
+	ConstructorOverloading(double b , int a)
+	{
+		x=a;
+		d=b;
+		System.out.println(x+d);	
+	}
+	
+
+	public static void main(String[] args)
+	{
+		ConstructorOverloading co = new ConstructorOverloading(20.5,10);
+		
+		//Note: co.ConstructorOverloading(100,200); gives error
+		
+	}
+}
