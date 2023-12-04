e1e942bbee3efc237a9970dd60f35a49526ec2c7
Gouse <professionalgouse123@gmail.com>
2023-12-04 14:15:07 +0530

    first commit

---
+++ src/finalkeyword/FinalVariable.java
@@ -0,0 +1,21 @@
+package finalkeyword;
+
+class Test 
+{
+	final int x =100;
+}
+
+public class FinalVariable
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Test t = new Test();
+		//t.x = 200;
+		System.out.println(t.x);
+		
+
+	}
+
+}
